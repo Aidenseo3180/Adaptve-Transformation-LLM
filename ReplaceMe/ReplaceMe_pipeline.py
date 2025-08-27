be2364c8f37f8253eb83a5854b70d0e4e9373c4f
Aiden <minwoo3180@gmail.com>
2025-08-27 18:45:57 -0400

    changes

--- ReplaceMe/ReplaceMe_pipeline.py
+++ ReplaceMe/ReplaceMe_pipeline.py
@@ -74,7 +74,7 @@ def ReplaceMe_pipeline(config):
         filtered_config['solver'] = config.get('solver', 'adam')  # VIB는 Adam 사용 권장
         
         # Load average distances and select non-overlapping blocks
-        average_distances = torch.load(filtered_config['distances_path'])  
+        average_distances = torch.load(filtered_config['distances_path'], weights_only=False)  
         selected_blocks = select_non_overlapping_blocks(
             average_distances, 
             filtered_config['layers_to_skip'], 
