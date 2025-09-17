70c31dbe908f37596d2326601987a4bf5b370df6
Aiden <minwoo3180@gmail.com>
2025-09-17 13:57:25 -0400

    added

--- ReplaceMe/ReplaceMe_pipeline.py
+++ ReplaceMe/ReplaceMe_pipeline.py
@@ -67,7 +67,7 @@ def ReplaceMe_pipeline(config):
         filtered_config = {k: v for k, v in config.items() if k in signature.parameters}
         
         # Load distances and select blocks
-        average_distances = torch.load(filtered_config['distances_path'], weights_only=False)
+        average_distances = torch.load(filtered_config['distances_path'], weights_only=True)
         selected_blocks = select_non_overlapping_blocks(
             average_distances,
             filtered_config['layers_to_skip'],
